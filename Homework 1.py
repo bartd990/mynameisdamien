0e4ea10b3ab4fb7e5dc15622b83bad8c49ed1bfc
Damien Bart <svusdroccksclub@gmail.com>
2019-05-04 12:30:41 -0700

    Extras

---
+++ Homework 1.py
@@ -0,0 +1,15 @@
+import turtle
+Window = turtle.Screen()
+Window.bgcolor("black")
+Window.title("Damien's super fun and boring game of code he did for homework")
+TurtlePen = turtle.Pen()
+turtle.speed(10)
+Window.colormode(255)
+TurtlePen.pencolor("white")
+for counter in range(9000):
+    TurtlePen.forward(counter)
+    TurtlePen.left(20)
+    if(counter > 50):
+        TurtlePen.pensize(15)
+    else:
+        TurtlePen.pensize(20)
