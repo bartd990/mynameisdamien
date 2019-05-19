be06ef11e054178c5f227bd2a99b8215422edc1b
Damien Bart <svusdroccksclub@gmail.com>
2019-05-19 11:40:16 -0700

    Add

---
+++ SayMyName.py
@@ -0,0 +1,6 @@
+your_name = input("What is your name?")
+for counter in range(100):
+    #print(your_name)
+    #print(your_name, end="")
+    #print(your_name, end="is the best!")
+    print(your_name, "is the best",end=" duh! at coding!")
