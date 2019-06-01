b49634034cb34111111375b42405678de6333de4
Damien Bart <svusdroccksclub@gmail.com>
2019-06-01 12:40:53 -0700

    Update ColorSpiralInput.py

--- ColorSpiralInput.py
+++ ColorSpiralInput.py
@@ -3,4 +3,19 @@ ColorSpiralInput.py -print a colorful spiral of users name
 """
 # inport turtle graphics
 import turtle
-T = Turtle.Pen()
+# T is for turtle pen
+T = turtle.Pen()
+turtle.bgcolor("red")
+# set up a list of 7 valid Python color names
+colors = ["black","yellow","blue","green","orange","violet","white"]
+# ask user for the number of sides, between 1 and 8, with a default of 4
+sides = int(turtle.numinput("Number of sides","How many sides do you want (1-8)?", 4, 1, 8))
+# draw a colorful spiral with user-specified number of sides
+for colorCounter in range(360):
+    # print their name folled by a space, not a new line
+    T.pencolor(colors[colorCounter % sides])
+    # changes the size to match the number of sides
+    T.forward(colorCounter*3 / sides+colorCounter)
+    # turn 360 / sides + 1 degrees
+    T.left(360 / sides + 1)
+    
