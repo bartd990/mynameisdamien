f2f1e7e6aead8bea7e4aac4d8ce3f5fdb271ef57
Damien Bart <svusdroccksclub@gmail.com>
2019-09-14 12:29:21 -0700

    hi

--- Pizza calculator/PizzaCalculator.py
+++ Pizza calculator/PizzaCalculator.py
@@ -7,7 +7,7 @@ class PizzaCalculator:
     meat_lovers_price = input("What is the price of a meat lovers pizza tonight? $")
     meat_lovers_price = eval(meat_lovers_price)
     # 2. ask costomer: which pizzas do they want?
-    pnumber_of_pepperonies = 0
+    number_of_pepperonies = 0
     number_of_hawaiians = 0
     number_of_meat_lovers = 0
     pizzas_selected = input("Customer, which pizzas do you want to order? (1. pepperoni, 2.hawaiian, 3.meat lover's)")
@@ -23,6 +23,7 @@ class PizzaCalculator:
     if("3" in pizzas_selected):
         number_of_meat_lovers = input("How many meat lover's pizza would you like to order?")
         number_of_meat_lovers = eval (number_of_meat_lovers)
+
     # 3. calculate order before tax = (order = price * quanity)
     pepperoni_order = number_of_pepperonies * pepperoni_price
     hawiian_order = number_of_hawaiians * hawiian_price
