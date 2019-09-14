125d93470d8cd37fa0c70e80829942bf5ff09e92
Damien Bart <svusdroccksclub@gmail.com>
2019-09-14 12:47:54 -0700

    hi

---
+++ Pizza calculator/PizzaCalculatorApp.py
@@ -0,0 +1,5 @@
+from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager
+from kivy.core.window import Window
+from ManagerScreen import ManagerScreen
